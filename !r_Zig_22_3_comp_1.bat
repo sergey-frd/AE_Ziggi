1d6d6edfe633d99bea1e2c31f639b6e709364b45
sergey-frd <sergey.frd@gmail.com>
2022-07-16 12:18:51 +0300

    220716
    220716

---
+++ !r_Zig_22_3_comp_1.bat
@@ -0,0 +1,11 @@
+c:
+cd c:\Program Files\Adobe\Adobe After Effects 2022\Support Files\
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Igor\\sf_IgorSvt22_1.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_Zig_22_3_comp_1.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_Zig_22_3_sport_1.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_Zig_22_3_urban_1.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_Zig_22_3_natr_1.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_Zig_22_3_roman_1.jsx"
+AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_Zig_22_3_diplom_1.jsx"
+
+Zig_22_3_comp_1
