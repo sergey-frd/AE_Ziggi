32d2815302d5d88c9129cefb229b30f77725b40d
sergey-frd <sergey.frd@gmail.com>
2022-08-27 12:15:26 +0300

    220827
    220827

--- !r_ae_System_2.bat
+++ !r_ae_System_2.bat
@@ -1,3 +1,3 @@
 c:
 cd c:\Program Files\Adobe\Adobe After Effects 2022\Support Files\
-AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_System_22_2.jsx"
+AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_System_22_2b.jsx"
---
+++ !r_ae_System_3.bat
@@ -0,0 +1,3 @@
+c:
+cd c:\Program Files\Adobe\Adobe After Effects 2022\Support Files\
+AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ziggi\\sf_System_22_3.jsx"
